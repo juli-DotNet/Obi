21fb9c8bccd3823b41beb63f514fc4df14a228ce
Julian Tabaku <julian.tabaku@outlook.com>
2022-01-08 10:21:22 +0100

    add web and infrastructure projects

---
+++ ObiMenagement.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ObiMenagement.Infrastructure;
+
+public class Class1
+{
+}
---
+++ ObiMenagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ObiMenagement.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
