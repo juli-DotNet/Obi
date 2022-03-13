f95cbf295c543a1253dbf36c6d7ab5aaf0a7e19e
julian <julian.tabaku@outlook.com>
2022-03-13 13:05:37 +0100

    add production settings

--- ObiMenagement.Core/Services/CityService.cs
+++ ObiMenagement.Core/Services/CityService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using ObiMenagement.Core.Common;
 using ObiMenagement.Core.Interfaces;
 using ObiMenagement.Core.Models;
@@ -7,20 +8,31 @@ namespace ObiMenagement.Core.Services;
 public class CityService : BaseService, ICityService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<CityService> logger;
 
-    public CityService(IUnitOfWork unitOfWork)
+    public CityService(IUnitOfWork unitOfWork, ILogger<CityService> logger)
     {
         _unitOfWork = unitOfWork;
+        this.logger = logger;
     }
     public async Task<Response> CreateAsync(City model)
     {
-        var result = new Response();
-        if (await ValidateModel(model, result)) return result;
+        try
+        {
+            var result = new Response();
+            if (await ValidateModel(model, result)) return result;
 
-        model.Country = await _unitOfWork.CountryRepository.FirstOrDefault(a => a.Id == model.Country.Id);
-        await _unitOfWork.CityRepository.InsertAsync(model);
-        await _unitOfWork.SaveChangesAsync();
-        return result;
+            model.Country = await _unitOfWork.CountryRepository.FirstOrDefault(a => a.Id == model.Country.Id);
+            await _unitOfWork.CityRepository.InsertAsync(model);
+            await _unitOfWork.SaveChangesAsync();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
+
     }
 
     private async Task<bool> ValidateModel(City model, Response result)
@@ -48,51 +60,93 @@ public class CityService : BaseService, ICityService
 
     public async Task<Response> DeleteAsync(int id)
     {
-        var result = new Response();
-        if (!await _unitOfWork.CityRepository.AnyAsync(a => a.Id ==id && a.IsValid))
+        try
         {
-            result.Exception = new ObiException(ErrorMessages.EntityDoesntExist(id));
+            var result = new Response();
+            if (!await _unitOfWork.CityRepository.AnyAsync(a => a.Id == id && a.IsValid))
+            {
+                result.Exception = new ObiException(ErrorMessages.EntityDoesntExist(id));
+                return result;
+            }
+
+            await _unitOfWork.CityRepository.DeleteAsync(id);
+            await _unitOfWork.SaveChangesAsync();
             return result;
         }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
 
-        await _unitOfWork.CityRepository.DeleteAsync(id);
-        await _unitOfWork.SaveChangesAsync();
-        return result;
     }
 
     public async Task<Response> EditAsync(City model)
     {
-        var result = new Response();
-        if (!await _unitOfWork.CountryRepository.AnyAsync(a => a.Id ==model.Id && a.IsValid))
+        try
         {
-            result.Exception = new ObiException(ErrorMessages.EntityDoesntExist(model.Id));
+            var result = new Response();
+            if (!await _unitOfWork.CountryRepository.AnyAsync(a => a.Id == model.Id && a.IsValid))
+            {
+                result.Exception = new ObiException(ErrorMessages.EntityDoesntExist(model.Id));
+                return result;
+            }
+            if (await ValidateModel(model, result)) return result;
+            model.Country = await _unitOfWork.CountryRepository.FirstOrDefault(a => a.Id == model.Country.Id);
+            await _unitOfWork.CityRepository.UpdateAsync(model);
+            await _unitOfWork.SaveChangesAsync();
             return result;
         }
-        if (await ValidateModel(model, result)) return result;
-        model.Country = await _unitOfWork.CountryRepository.FirstOrDefault(a => a.Id == model.Country.Id);
-        await _unitOfWork.CityRepository.UpdateAsync(model);
-        await _unitOfWork.SaveChangesAsync();
-        return result;
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
+
     }
 
     public async Task<Response<IEnumerable<City>>> GetAllAsync()
     {
-        var result = new Response<IEnumerable<City>>();
+        try
+        {
+            var result = new Response<IEnumerable<City>>();
 
-        result.Result = await _unitOfWork.CityRepository.WhereAsync(a=>true, a=>a.Country);
-        return result;
+            result.Result = await _unitOfWork.CityRepository.WhereAsync(a => true, a => a.Country);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
     }
     public async Task<Response<IEnumerable<City>>> GetAllWithoutMetadataAsync(long countryId)
     {
-        var result = new Response<IEnumerable<City>>();
+        try
+        {
+            var result = new Response<IEnumerable<City>>();
 
-        result.Result = await _unitOfWork.CityRepository.WhereAsync(a => a.Country.Id == countryId);
-        return result;
+            result.Result = await _unitOfWork.CityRepository.WhereAsync(a => a.Country.Id == countryId);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
     }
     public async Task<Response<City>> GetByIdAsync(int id)
     {
-        var result = new Response<City>();
-        result.Result = await _unitOfWork.CityRepository.FirstOrDefault(a=>a.Id==id,a=>a.Country);
-        return result;
+        try
+        {
+            var result = new Response<City>();
+            result.Result = await _unitOfWork.CityRepository.FirstOrDefault(a => a.Id == id, a => a.Country);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            throw;
+        }
     }
 }
