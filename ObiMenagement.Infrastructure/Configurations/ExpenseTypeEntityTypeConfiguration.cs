06ca3ecccfb5b142b4c6cdbc1cf884a4f2dd821d
julian <julian.tabaku@outlook.com>
2022-03-18 07:35:37 +0100

    configure the Value object of ExpenseType entity

---
+++ ObiMenagement.Infrastructure/Configurations/ExpenseTypeEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ObiMenagement.Core.Models;
+
+namespace ObiMenagement.Infrastructure.Configurations;
+
+public class ExpenseTypeEntityTypeConfiguration : IEntityTypeConfiguration<ExpenseType>
+{
+    public void Configure(EntityTypeBuilder<ExpenseType> builder)
+    {
+        builder
+            .Property(b => b.Name).HasMaxLength(200)
+            .IsRequired();
+        builder.OwnsOne(b => b.DefaultPayment);
+    }
+}
---
+++ ObiMenagement.Infrastructure/Migrations/20220318063300_ClientModels.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ObiMenagement.Infrastructure.Migrations
+{
+    public partial class ClientModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: false),
+                    LocationId = table.Column<int>(type: "integer", nullable: false),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedById = table.Column<string>(type: "text", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedById = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Client_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Client_AspNetUsers_ModifiedById",
+                        column: x => x.ModifiedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Client_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    DefaultPayment_Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    DefaultPayment_CurrencyId = table.Column<int>(type: "integer", nullable: false),
+                    DefaultPayment_PaymentTypeEnum = table.Column<int>(type: "integer", nullable: false),
+                    IsFuel = table.Column<bool>(type: "boolean", nullable: false),
+                    IsPositive = table.Column<bool>(type: "boolean", nullable: false),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedById = table.Column<string>(type: "text", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedById = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExpenseType_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExpenseType_AspNetUsers_ModifiedById",
+                        column: x => x.ModifiedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExpenseType_Currency_DefaultPayment_CurrencyId",
+                        column: x => x.DefaultPayment_CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientContact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PersonId = table.Column<int>(type: "integer", nullable: false),
+                    ClientId = table.Column<int>(type: "integer", nullable: false),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedById = table.Column<string>(type: "text", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedById = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientContact", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientContact_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ClientContact_AspNetUsers_ModifiedById",
+                        column: x => x.ModifiedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ClientContact_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClientContact_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoadClient",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClientId = table.Column<int>(type: "integer", nullable: false),
+                    StartingLocationId = table.Column<int>(type: "integer", nullable: false),
+                    DestinationLocationId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    CurrencyId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    WeightInKg = table.Column<decimal>(type: "numeric", nullable: false),
+                    VolumeInM3 = table.Column<decimal>(type: "numeric", nullable: false),
+                    LDM = table.Column<decimal>(type: "numeric", nullable: false),
+                    Other = table.Column<string>(type: "text", nullable: false),
+                    IsValid = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedById = table.Column<string>(type: "text", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedById = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoadClient", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoadClient_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_RoadClient_AspNetUsers_ModifiedById",
+                        column: x => x.ModifiedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_RoadClient_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoadClient_Currency_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoadClient_Location_DestinationLocationId",
+                        column: x => x.DestinationLocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoadClient_Location_StartingLocationId",
+                        column: x => x.StartingLocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_CreatedById",
+                table: "Client",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_LocationId",
+                table: "Client",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_ModifiedById",
+                table: "Client",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientContact_ClientId",
+                table: "ClientContact",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientContact_CreatedById",
+                table: "ClientContact",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientContact_ModifiedById",
+                table: "ClientContact",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientContact_PersonId",
+                table: "ClientContact",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseType_CreatedById",
+                table: "ExpenseType",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseType_DefaultPayment_CurrencyId",
+                table: "ExpenseType",
+                column: "DefaultPayment_CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseType_ModifiedById",
+                table: "ExpenseType",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_ClientId",
+                table: "RoadClient",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_CreatedById",
+                table: "RoadClient",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_CurrencyId",
+                table: "RoadClient",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_DestinationLocationId",
+                table: "RoadClient",
+                column: "DestinationLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_ModifiedById",
+                table: "RoadClient",
+                column: "ModifiedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadClient_StartingLocationId",
+                table: "RoadClient",
+                column: "StartingLocationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientContact");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseType");
+
+            migrationBuilder.DropTable(
+                name: "RoadClient");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
